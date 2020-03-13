eafe146f8c267b8452555a4446e191f56d4a8c5d
5p4r70n <jothishwp8@outlook.com>
2020-03-13 14:08:22 +0530

    added updated logic

--- pythonProject/FanDep.py
+++ pythonProject/FanDep.py
@@ -1,42 +1,28 @@
-# fanName=['usha','bajaj','crompten']
-# fanNo=[100,100,100]
-
-# def names (no):
-#     for x in range(1,no+1):
-#         fanName.append(raw_input('enter the '+str(x) +' name : '))
-#         fanNo.append(raw_input('enter the count : '))
-
-# iP= int(raw_input('Enter How many type you have ? : '))
-# names(iP)
-# data = zip(fanName,fanNo)
-
-data = {'usha' : 100,'bajaj': 100,'crompton' : 100} #dictionary
-
-dependName = ['usha','bajaj','crompton'] #dependancy usha 2-> bajaj 4 -> crompton 5
-dependCount = [2,3,4]
-printList= []
-day1 = 100 #need to install 100 fans in a day
-
-def ror (name,no):
-    for x in range(1,no+1):
-        printList.append(name) #add names to the print list
-        data[name]=data[name]-1    #this will minus 1 from each time adding to print list 
-        if len(printList)==day1: 
-            break
+#in this logic fan dependency order should be  ordered in dic   
 
 
-for x in range (0,day1):
-    for y in range(0,(len(dependName))):
-        ror(dependName[y],dependCount[y])
-        if len(printList)==day1:
-            break
-    if len(printList)==day1:
-            break
+data = ['f1','f2','f3','f4'] #dictionary
+
+dependency = {'f1':['f2','f3','f4'],'f2':['f3','f4'],'f3':['f4']} #dependacy
+printList= [] #fan order saves here
+
+def findDepen ():
+    for x in range (0,len(data)): 
+        if data[x] not in printList: #for skiping printlist alredy in items
+            a=dependency[data[x]] #get list from dependency pointer to a variable
+            if a[0] == printList[len(printList)-1]: # checking printList last elemnt == 1st element of dependency 
+                printList.append(data[x]) 
+
+for x in range (0,len(data)):
+    if data[x] not in dependency: #to find not avilable charecter in dependancy that is the 1st 
+        printList.append(data[x])
 
 
-data['usha']=data['usha']-10
-print ('you need to install your todays '+ str(day1) + ' fans in this order ')
-print  (printList)
-print ('remeaning fans in hand as follows')
-print (data)
+while True:
+    findDepen()
+    if(len(data) == len(printList)): #fan number and fan order number equals loop break 
+        break
+
+print (printList)
+
 

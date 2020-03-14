4ddf5f25e84b61ca0bde23315ce5037d3c6c3c52
5p4r70n <jothishwp8@outlook.com>
2020-03-14 15:20:37 +0530

    added new strategy :D

--- pythonProject/FanDep.py
+++ pythonProject/FanDep.py
@@ -9,9 +9,8 @@ printList= [] #fan order saves here
 def findDepen ():
     for x in range (0,len(data)): 
         if data[x] not in printList: #for skiping printlist alredy in items
-            a=dependency[data[x]] #get list from dependency pointer to a variable
-            if a[0] == printList[len(printList)-1]: # checking printList last elemnt == 1st element of dependency 
-                printList.append(data[x]) 
+            if (set(printList)==set(dependency[data[x]])) == True: # this will cehck the two lists are equal or not
+                printList.append(data[x])
 
 for x in range (0,len(data)):
     if data[x] not in dependency: #to find not avilable charecter in dependancy that is the 1st 
@@ -24,5 +23,3 @@ while True:
         break
 
 print (printList)
-
-
